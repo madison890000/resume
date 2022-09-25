6639df932c002a0b75cdf62c46399324c1c7ad1c
madison890000 <madison.sss.ma@gmail.com>
2022-09-26 00:57:08 +0800

    fix code smell

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from 'react';
 import styles from './index.module.scss'
 
-type TagType = 'filled' | 'normal'| 'less';
+type TagType = 'filled' | 'normal' | 'less';
 
 interface TagProps {
     type?: TagType
@@ -13,17 +13,14 @@ const Tag: React.FC<PropsWithChildren<TagProps>> = ({ type = 'normal', children 
             return (
                 <span className={styles.filled}>{children}</span>
             );
-            break;
         case 'normal':
             return (
                 <span className={styles.normal}>{children}</span>
             );
-            break;
         case 'less':
             return (
                 <span className={styles.less}>{children}</span>
             );
-            break;
         default:
             return (
                 <span>{children}</span>
