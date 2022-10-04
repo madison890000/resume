cdd79999ea349b3e53c95afc9c90245744fd4f46
madison <madison.sss.ma@gmail.com>
2022-10-04 23:41:39 +0800

    feature: add dark mode (#13)
    * feature: add dark mode
    
    * update test cases
    
    * fixed code smell
    
    * ignore mocks folders

---
+++ src/components/Capability/__mocks__/index.tsx
@@ -0,0 +1,7 @@
+// Import this named export into your test file:
+const mock = jest.fn()
+    .mockImplementation(() => {
+        return () => <div>Mock Capability</div>;
+    });
+
+export default mock;
