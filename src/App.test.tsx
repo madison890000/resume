029a6f05d037a125e2b0d233cb48d6f99b6351e5
madison890000 <madison.sss.ma@gmail.com>
2022-10-04 23:26:20 +0800

    update test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
+import Capability from './components/Capability';
 
-xit('renders App', () => {
+jest.mock('./components/Capability'); // Capability is now a mock constructor
+
+beforeEach(() => {
+    // Clear all instances and calls to constructor and all methods:
+    // @ts-ignore
+    Capability.mockClear();
+});
+it('renders App', () => {
     const AppDom = renderer.create(<App />)
         .toJSON();
     expect(AppDom)
