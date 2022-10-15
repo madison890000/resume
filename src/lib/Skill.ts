80a0c0da75047a7552605b50ad12054afb7b1bcd
madison890000 <madison.sss.ma@gmail.com>
2022-10-15 12:40:20 +0800

    fix react warning

--- src/lib/Skill.ts
+++ src/lib/Skill.ts
@@ -1,13 +1,15 @@
 import { Importance, SkillLevel } from './types';
-
+import { v4 } from 'uuid';
 
 export default class Skill {
     public name: string;
     public level: SkillLevel;
     public importance: Importance;
     public ages: number;
+    id: string;
 
     constructor(name: string, level: SkillLevel, ages: number, importance: Importance) {
+        this.id = v4();
         this.name = name;
         this.level = level;
         this.ages = ages;
