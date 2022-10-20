1e1998be854d7cf7343e81f3fc53d35f7d3c8251
madison890000 <madison.sss.ma@gmail.com>
2022-10-21 00:24:26 +0800

    add eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'printWidth': 200,
+    'tabWidth': 4,
+    'useTabs': false,
+    'semi': true,
+    'singleQuote': true,
+    'bracketSpacing': true,
+    'arrowParens': 'avoid',
+    'trailingComma': 'none',
+}
